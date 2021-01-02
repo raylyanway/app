78417a7e859517339baf3e4807d2b7a3bd025ea9
iLya Raylyan <IRaylyan@alfabank.ru>
2021-01-02 22:52:22 +0300

    feat(app): create server

---
+++ index.js
@@ -0,0 +1,9 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.end('Hello world!\n');
+})
+
+server.listen(3000, () => {
+    console.log('The server is listening on port 3000 now')
+})
